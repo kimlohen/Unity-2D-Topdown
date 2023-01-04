616e4b7362cf9bcf779953442899ec070bc1a995
Minsu Kim <qmdlrhdfyd@naver.com>
2023-01-04 16:17:13 +0900

    Feature: Implement Talk System

--- My project/Assets/Script/GameManager.cs
+++ My project/Assets/Script/GameManager.cs
@@ -5,24 +5,44 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
     public GameObject talkPanel;
+    public Image portraitImg;
     public Text talkText;
     public GameObject scanObject;
     public bool isAction;
+    public int talkIndex;
 
     public void Action(GameObject scanObj)
     {   
-        if(isAction) { // Exit Action
-            isAction = false;
-        }
-        else { // Enter Action
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것의 이름은 " + scanObj.name + "이라고 한다.";
-        }
-        
+        scanObject = scanObj;
+        ObjData objData = scanObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
+
         talkPanel.SetActive(isAction);
     }
 
+    void Talk(int id, bool isNpc)
+    {
+        string talkData = talkManager.GetTalk(id, talkIndex);
 
+        if(talkData == null){
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+
+        if(isNpc){
+            talkText.text = talkData.Split(':')[0];
+            portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImg.color = new Color(1, 1, 1, 1);
+        }
+        else{
+            talkText.text = talkData;
+            portraitImg.color = new Color(1, 1, 1, 0);
+        }
+
+        isAction = true;
+        talkIndex++;
+    }
 }
