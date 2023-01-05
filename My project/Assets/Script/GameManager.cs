7edd2a90e61595f6d5af910483b49d7ecce36c28
Minsu Kim <qmdlrhdfyd@naver.com>
2023-01-06 00:44:14 +0900

    Feature: Implement Talk Animation

--- My project/Assets/Script/GameManager.cs
+++ My project/Assets/Script/GameManager.cs
@@ -7,9 +7,11 @@ public class GameManager : MonoBehaviour
 {
     public TalkManager talkManager;
     public QuestManager questManager;
-    public GameObject talkPanel;
+    public Animator talkPanel;
+    public Animator portraitAnim;
     public Image portraitImg;
-    public Text talkText;
+    public Sprite prevPortrait;
+    public TypeEffect talk;
     public GameObject scanObject;
     public bool isAction;
     public int talkIndex;
@@ -27,14 +29,24 @@ public class GameManager : MonoBehaviour
         Talk(objData.id, objData.isNpc);
 
         // Visible Talk for Action
-        talkPanel.SetActive(isAction);
+        talkPanel.SetBool("isShow", isAction);
     }
 
     void Talk(int id, bool isNpc)
     {
+        int questTalkIndex;
+        string talkData;
+
         // Set Talk Data
-        int questTalkIndex = questManager.GetQuestTalkIndex(id);
-        string talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        if (talk.isAnim) {
+            talk.SetMsg("");
+            return;
+        }
+
+        else {
+            questTalkIndex = questManager.GetQuestTalkIndex(id);
+            talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        }
 
         // End Talk
         if(talkData == null){
@@ -46,12 +58,21 @@ public class GameManager : MonoBehaviour
 
         //Continue Talk
         if(isNpc){
-            talkText.text = talkData.Split(':')[0];
+            talk.SetMsg(talkData.Split(':')[0]);
+
+            //Show Portrait
             portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
             portraitImg.color = new Color(1, 1, 1, 1);
+
+            //Animation Portrait
+            if (prevPortrait != portraitImg.sprite){
+                portraitAnim.SetTrigger("doEffect");
+                prevPortrait = portraitImg.sprite;
+            }
         }
         else{
-            talkText.text = talkData;
+            talk.SetMsg(talkData);
+
             portraitImg.color = new Color(1, 1, 1, 0);
         }
 
---
+++ My project/Assets/Script/TypeEffect.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeEffect : MonoBehaviour
+{
+    public int CharPerSeconds;
+    public GameObject EndCursor;
+    public bool isAnim;
+
+    Text msgText;
+    AudioSource audioSource;
+    string targetMsg;
+    int index;
+    float interval;
+
+    private void Awake()
+    {
+        msgText = GetComponent<Text>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void SetMsg(string msg)
+    {
+        if(isAnim){
+            msgText.text = targetMsg;
+            CancelInvoke();
+            EffectEnd();
+        }
+        else{
+            targetMsg = msg;
+            EffectStart();
+        }
+    }
+
+    void EffectStart()
+    {
+        msgText.text = "";
+        index = 0;
+        EndCursor.SetActive(false);
+
+        // Start Animation
+        interval = 1.0f / CharPerSeconds;
+        isAnim = true;
+        Invoke("Effecting", interval);
+    }
+
+    void Effecting()
+    {
+        // End Animantion
+        if(msgText.text == targetMsg){
+            EffectEnd();
+            return;
+        }
+
+        msgText.text += targetMsg[index];
+
+        // Sound
+        if (targetMsg[index] != ' ' || targetMsg[index] != '.')
+            audioSource.Play();
+
+        index++;
+
+        
+        //Recursive Animation
+        Invoke("Effecting", interval);
+    }
+
+    void EffectEnd()
+    {
+        isAnim = false;
+        EndCursor.SetActive(true);
+    }
+
+}
